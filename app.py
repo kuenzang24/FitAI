6f941f3f1382a6b138521e1c69b0dfa4824eedaa
kuenzang24.dev <kuenzang24.dev@gmail.com>
2024-05-21 17:35:20 +0600

    added the pages and images

--- app.py
+++ app.py
@@ -3,8 +3,16 @@ from flask import Flask,render_template,request
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template("index.html",value=["FitAI"])
+def landing_Page():
+    return render_template("index.html")
+
+@app.route("/home")
+def home_page():
+    return render_template("home.html")
+
+@app.route("/input")
+def input():
+    return "<p>Output</p>"
 
 if __name__ == "__main__":
     app.run(debug=True)
